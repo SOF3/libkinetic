d1b18d51e8f700af9961099a81228ca7ebe6bbfc
SOFe <sofe2038@gmail.com>
2018-09-28 13:58:00 +0800

    Added <wizard> parsing

--- libkinetic/src/SOFe/Libkinetic/Wizard/WizardComponent.php
+++ libkinetic/src/SOFe/Libkinetic/Wizard/WizardComponent.php
@@ -25,11 +25,24 @@ namespace SOFe\Libkinetic\Wizard;
 use Generator;
 use SOFe\Libkinetic\Base\IdComponent;
 use SOFe\Libkinetic\Base\KineticComponent;
+use SOFe\Libkinetic\Parser\Child\ChildNodeRouter;
 use SOFe\Libkinetic\UI\Group\UiGroupComponent;
+use SOFe\Libkinetic\Variable\ReturnComponent;
+use SOFe\Libkinetic\Variable\VarDeclarationComponent;
 
 class WizardComponent extends KineticComponent{
+	/** @var VarDeclarationComponent[] */
+	protected $params = [];
+	/** @var ReturnComponent[] */
+	protected $returns = [];
+
 	public function getDependencies() : Generator{
 		yield UiGroupComponent::class;
 		yield new IdComponent(true);
+	}
+
+	public function acceptChildren(ChildNodeRouter $router) : void{
+		$router->acceptMulti("param", VarDeclarationComponent::class, $this->params, 0);
+		$router->acceptMulti("return", ReturnComponent::class, $this->returns, 0);
 	}
 }
